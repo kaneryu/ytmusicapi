a5cc6cbd7ce807bb520c69d93eebdca5ad604700
sigma67 <16363825+sigma67@users.noreply.github.com>
2024-07-24 19:37:33 +0200

    add get_user_videos (#632)
    * add get_user_videos
    
    * add to docs

--- example.py
+++ example.py
@@ -8,5 +8,10 @@ async def main():
     async with ytm._session:
         results = await asyncio.gather(*[ytm.search(artist, filter="songs", limit=1) for artist in artists])
         print(*[f"{result[0]['artists'][0]['name']}: {result[0]['title']}" for result in results], sep="\n")
+        
+        get_user = await ytm.get_user("UCDyuTkFC49GDHC_nUviOEag")
+        results = await ytm.get_user_videos("UCDyuTkFC49GDHC_nUviOEag", get_user["videos"]["params"])
+        print("User Videos:")
+        print(results)
 
 asyncio.run(main())
